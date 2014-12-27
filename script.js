62f25fdee437c3326833c9ad3cf531e6fcd1fddc
Vick003 <eapvic@gmail.com>
2014-12-27 15:41:49 -0800

    Implemented Thi's suggestions from Mboutique on Ajax CAll for this one.

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ $('.list-organize a').on('click', function(e) {
 	e.preventDefault();
 	var url = this.href;
 	
-	var link_pos = url.lastIndexOf('/');
-	var link = url.slice(link_pos + 1);
+	var urlArray = url.split('/');
+	console.log(urlArray);
+	var link = urlArray[urlArray.length-1]
 	$('title').text(link);
 
 	$('.list-organize a.current').removeClass('current');         // Clear current indicator
     $(this).addClass('current');
 
-	$('#container').remove();
+	
 	$('#content').load(url + ' #content').hide().fadeIn('fast');
 });
